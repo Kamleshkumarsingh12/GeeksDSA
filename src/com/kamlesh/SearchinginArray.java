54bf5224d745ba192a3d3d191702ef6e9d18c5f0
Kamleshkumarsingh12 <92987383+Kamleshkumarsingh12@users.noreply.github.com>
2022-06-13 20:39:26 +0530

    SearchinginArray.java

--- src/com/kamlesh/SearchinginArray.java
+++ src/com/kamlesh/SearchinginArray.java
@@ -4,9 +4,9 @@ package com.kamlesh;
 public class SearchinginArray {
     public static void main(String[] args) {
         int[] arr = {20,15,35,63,42};
-        int n = arr.length;
-        int key = 42;
-        int position = search(arr,n,key);
+        int n = arr.length;     //n = length of array
+        int key = 42;           //key is the value we need to search in array
+        int position = search(arr,n,key);       //calling the method
 
         if(position==-1)
         {
@@ -17,16 +17,19 @@ public class SearchinginArray {
 
     }
 
+    //creating a method to search an element in array
     public static int search(int[] arr, int n, int key)
     {
         for (int i = 0; i < arr.length; i++)
         {
+            //iterating to determine the array element at index i is key element
             if(arr[i] == key)
             {
                 return i;
             }
 
         }
+        //if the key is not found
         return -1;
     }
 }
